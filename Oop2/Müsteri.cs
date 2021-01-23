4c80f13669d73d15cf7f3a3c586fc18784bf7db2
Deniz ASLAN <deniz.aslan@yilport.com>
2021-01-23 21:25:27 +0300

    23.01.2021

---
+++ Oop2/Müsteri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    class Müsteri
+    {
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string SirketAdi { get; set; }
+        public string TcNo { get; set; }
+        public string VergiNo { get; set; }
+
+    }
+}
---
+++ Oop2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
