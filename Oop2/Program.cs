5714de28d6d8b31c2e9f942a46f0f928ddf733b7
Deniz ASLAN <deniz.aslan@yilport.com>
2021-01-25 09:39:03 +0300

    25.01.2021

--- Oop2/Program.cs
+++ Oop2/Program.cs
@@ -6,7 +6,14 @@ namespace Oop2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Müsteri müsteri1 = new Müsteri();
+            müsteri1.Adi = "Deniz";
+            müsteri1.Soyadi = "Aslan";
+            müsteri1.Id = 1;
+            müsteri1.TcNo = "111111111";
+            müsteri1.MusteriNo = "123456";
+            müsteri1.SirketAdi = "Yilport";
+            
         }
     }
 }
